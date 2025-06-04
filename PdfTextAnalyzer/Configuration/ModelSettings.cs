36d75da3eb595ed6dbf3f20415d10a15f036486d
Antonis Geralis <b3lievem@yandex.com>
2025-06-04 04:53:05 +0300

    small changes

--- PdfTextAnalyzer/Configuration/ModelSettings.cs
+++ PdfTextAnalyzer/Configuration/ModelSettings.cs
@@ -3,6 +3,6 @@ namespace PdfTextAnalyzer.Configuration;
 public class ModelSettings
 {
     public string ModelName { get; set; } = string.Empty;
-    public int MaxTokens { get; set; } = 1000;
+    public int MaxTokens { get; set; } = 4000;
     public float Temperature { get; set; } = 0.0f;
 }
