36abd4e580cec7a9fb4573e9bdbe313f66e82ee4
Antonis Geralis <b3lievem@yandex.com>
2025-06-12 23:04:44 +0300

    fix slop

--- PdfAiEvaluation/Models/EvaluationTestData.cs
+++ PdfAiEvaluation/Models/EvaluationTestData.cs
@@ -7,15 +7,15 @@ public class EvaluationTestData
 {
     public string TestId { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public List<ChatMessage> Messages { get; set; } = [];
+    public List<ChatMessage> Messages { get; set; } = new();
     public string? GroundTruth { get; set; }
     public string? GroundingContext { get; set; }
-    public Dictionary<string, object> Metadata { get; set; } = [];
+    public List<string>? Tags { get; set; }
 }
 
 public class EvaluationTestSet
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public List<EvaluationTestData> TestCases { get; set; } = [];
+    public List<EvaluationTestData> TestCases { get; set; } = new();
 }
