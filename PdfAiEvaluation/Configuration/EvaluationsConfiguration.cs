4892005059ab6508fd8498b761ddeddc5501396e
Antonis Geralis <b3lievem@yandex.com>
2025-06-13 12:15:13 +0300

    Fix up

--- PdfAiEvaluation/Configuration/EvaluationsConfiguration.cs
+++ PdfAiEvaluation/Configuration/EvaluationsConfiguration.cs
@@ -4,13 +4,11 @@ public class EvaluationsConfiguration
 {
     public const string SectionName = "Evaluations";
 
-    public List<EvaluationSettings> Evaluations { get; set; } = new();
-
     public EvaluationSettings? GetEvaluationByName(string name)
     {
-        return Evaluations.FirstOrDefault(e =>
+        return this.FirstOrDefault(e =>
             string.Equals(e.ExecutionName, name, StringComparison.OrdinalIgnoreCase));
     }
 
-    public bool HasEvaluations => Evaluations.Count > 0;
+    public bool HasEvaluations => this.Count > 0;
 }
